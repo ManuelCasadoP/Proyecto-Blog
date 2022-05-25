85614b5a33a1d07cf6f54f7a1f9967c0406518f1
warjade <warjade@yahoo.es>
2022-05-25 13:39:20 +0200

    mejorado Css Redactar Noticia

--- frontend/blog/src/components/WriteNews/WriteNews.jsx
+++ frontend/blog/src/components/WriteNews/WriteNews.jsx
@@ -2,23 +2,43 @@ import Header from "../Header/Header";
 import "./writeNews.css";
 
 function WriteNews() {
+
+    /**
+ *  JSON POST
+ */
+
+async function post(url, data) {
+    const response = await fetch(
+        url,
+        {
+            method: 'POST',
+            body: data,
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+    );
+    const responseData = await response.json();
+    return responseData;
+}
    
     return (
         <>
             <Header/>
             <div className="writeNewsContainer">
-                <label htmlFor="title"> Titulo </label>
-                <input type="text" />
-                <label htmlFor="date"> Fecha </label>
-                <input type="text" />
-                <label htmlFor="author"> Autor </label>
-                <input type="text" />
-                <label htmlFor="summary"> Sumario </label>
-                <input type="text" />
-                <label htmlFor="content"> Contenido </label>
-                <input type="text" />
-                <label htmlFor="src"> Imagenes </label>
-                <input type="text" />
+                <label className="writeNewsLabel" htmlFor="title"> Titulo </label>
+                <input className="writeNewsInput" type="text" />
+                <label className="writeNewsLabel" htmlFor="date"> Fecha </label>
+                <input className="writeNewsInput" type="text" />
+                <label className="writeNewsLabel" htmlFor="author"> Autor </label>
+                <input className="writeNewsInput" type="text" />
+                <label className="writeNewsLabel" htmlFor="summary"> Sumario </label>
+                <input className="writeNewsInput"type="text" />
+                <label className="writeNewsLabel" htmlFor="content"> Contenido </label>
+                <textarea className="writeNewsInput" rows="10" cols="20"></textarea>
+                <label className="writeNewsLabel" htmlFor="src"> Imagenes </label>
+                <input className="writeNewsInput" type="file" />
+                <button className="writeNewsButton">Enviar Noticia</button>
             </div>
         </>
     )

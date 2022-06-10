1b9dc982b0a8489b3def9d7311de3f920c39614c
Manuel Casado <ManuelCasado@protonmail.com>
2022-06-10 09:43:26 +0200

    Añadiendo console.log

--- backend/src/middleware/authMiddleware.mjs
+++ backend/src/middleware/authMiddleware.mjs
@@ -6,19 +6,32 @@ import { json } from 'express';
 export default function authMiddleware (request, response, next) {
     try {
         const [ method, token ] = request.headers.authorization.split(" ")
+        console.log()
         console.log(method);
+        console.log()
         console.log(token);
+        console.log()
         console.log("Hola");
+        console.log()
         
         const validAuth = jwt.verify(token, process.env.TOKEN_KEY)
         if ( validAuth ) {
             //const { username } = jwt.decode(token)
             //res.locals["username"] = username // Paso el username para uso en los controllers
 
-            next()
-
             const validacion = JSON.stringify(validAuth);
             console.log(`validAuth: ${validacion}`)
+            console.log()
+            console.log("Sigo en el middleware")
+            console.log()
+            const decodedToken = jwt.decode(token)
+            console.log(decodedToken)
+            console.log()
+            console.log("Salgo del Middleware")
+            console.log()
+            console.log()
+
+            next()            
             
         } else {
             console.log("Token no válido")}

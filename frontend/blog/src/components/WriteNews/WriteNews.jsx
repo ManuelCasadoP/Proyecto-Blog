83576b725f9bcb94b4277171822c9e7ac4817a2c
warjade <warjade@yahoo.es>
2022-06-21 13:44:57 +0200

    reparar ruta write

--- frontend/blog/src/components/WriteNews/WriteNews.jsx
+++ frontend/blog/src/components/WriteNews/WriteNews.jsx
@@ -3,11 +3,14 @@ import "./writeNews.css";
 import{ useState } from 'react';
 import swal from 'sweetalert';
 import host from "../../js/host.mjs";
+import { useNavigate } from "react-router-dom";
 
 
 export default function WriteNews () {	
 
 	//const host = "http://localhost:4000/api/v0.0/news";
+
+	const navigate = useNavigate()
 
 	const [ error, setError ] = useState(false);
 	const [ news, setNews ] = useState ({
@@ -94,8 +97,8 @@ export default function WriteNews () {
 						icon: "success",
 					})
 					.then(ok => {
-						if (ok) {document.location.href = '/write';		
-						}
+						//if (ok) {document.location.href = '/write'};
+						if (ok) navigate('/write/');
 					});
 					
 				} else if (response.status === 401){	
